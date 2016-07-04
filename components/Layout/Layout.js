e924475821948e834af8d2d1d8f330e1feeae4ae
Charing Wong <miss@charingwong.com>
2016-07-04 13:32:07 -0400

    Bunch of CSS tweaks
    Update navigation link padding
    Move solid background from [Index]Layout components to Hero component
    Remove image from Layout component and set fill colour in Header

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,19 +3,12 @@ import Header from './Header';
 import MainContent from './MainContent';
 import '../../static/styles/base.css';
 import s from './Layout.css';
-import heroImage from './hero-30.jpg';
 
 class Layout extends React.Component {
   render() {
-    // For some reason, background images only work if I set them
-    // inline. Setting this in the stylesheet (./Hero.css) didn't work
-    let heroBackgroundInlineCSS = {
-      backgroundImage: "url(" + heroImage + ")"
-    };
-
     return (
-      <div className={s.container} style={heroBackgroundInlineCSS}>
-        <Header />
+      <div className={s.container}>
+        <Header useLightFill={false} />
         <MainContent>
           {this.props.children}
         </MainContent>
