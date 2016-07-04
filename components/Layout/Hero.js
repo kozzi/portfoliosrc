608cc979b59c9bde8ee9280564fbcca7d17db274
Charing Wong <miss@charingwong.com>
2016-07-04 12:32:12 -0400

    Wrap .hero div in a .heroContainer div

--- components/Layout/Hero.js
+++ components/Layout/Hero.js
@@ -6,14 +6,16 @@ import s from './Hero.css';
 class Hero extends React.Component {
   render() {
     return (
-      <div className={s.hero}>
-        <Header {...this.props} />
-        <HeroCaption />
-        <h1>Charing</h1>
+      <div id="heroContainer" className={s.heroContainer}>
+        <div className={s.hero}>
+          <Header {...this.props} />
+          <HeroCaption />
+          <h1>Charing</h1>
 
-        <div className={s.tagline}>
-          <p>I design & code. I climb mountains for fun.</p>
-          <p>Hello from Toronto Canada, eh &hearts;</p>
+          <div className={s.tagline}>
+            <p>I design & code. I climb mountains for fun.</p>
+            <p>Hello from Toronto Canada, eh &hearts;</p>
+          </div>
         </div>
       </div>
     );
