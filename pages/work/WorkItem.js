bd3d9318261907728d2df4fc1b80e4a625bd5d53
Charing Wong <miss@charingwong.com>
2016-07-19 23:38:52 -0400

    Finish work page and WorkItem design

--- pages/work/WorkItem.js
+++ pages/work/WorkItem.js
@@ -1,14 +1,28 @@
 import React, { PropTypes } from 'react';
+import Link from '../../components/Link';
 import s from './WorkItem.css';
+import defaultImage from '../../static/images/rainier.jpg';
 
 const WorkItem = (props) =>
-  <section className={s.hello}>
-    {props.children}
+  <section className={s.workItem}>
+    <Link to={props.projectPath}>
+      <img src={props.heroImagePath} alt="Project thumbnail" />
+      <div className={s.blurb}>
+        {props.children}
+      </div>
+    </Link>
   </section>
 ;
 
 WorkItem.propTypes = {
+  projectPath: PropTypes.string.isRequired,
+  heroImagePath: PropTypes.string.isRequired,
   children: PropTypes.node,
+};
+
+WorkItem.defaultProps = {
+  projectPath: '/work',
+  heroImagePath: defaultImage,
 };
 
 export default WorkItem;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import Link from '../../components/Link';
 import WorkItem from './WorkItem';
+import scandiImage from './scandi/hero2.png';
+import pawndaImage from './pawnda/hero.png';
+import deweyImage from './dewey/hero.jpg';
+import switchingImage from './switching/switching.jpg';
+import snappingImage from './snapping/snapping.jpg' ;
 
 const WorkPage = (props) =>
   <Layout {...props}>
-    <h1>Work</h1>
-    <WorkItem>
-      <p><Link to="/work/scandi">Scandi</Link></p>
+    <WorkItem projectPath="/work/scandi" heroImagePath={scandiImage}>
+      <h2>Team Scandi</h2>
+      <p>Illustration, YouTube channel design</p>
     </WorkItem>
-    <WorkItem>
-      <p><Link to="/work/pawnda">Pawnda</Link></p>
+    <WorkItem projectPath="/work/pawnda" heroImagePath={pawndaImage}>
+      <h2>Kalashnikov Pawn-da</h2>
+      <p>Illustration, YouTube channel design</p>
     </WorkItem>
-    <WorkItem>
-      <p><Link to="/work/dewey">Dewey</Link></p>
+    <WorkItem projectPath="/work/dewey" heroImagePath={deweyImage}>
+      <h2>Dewey</h2>
+      <p>iOS development (Swift), Interaction design, Illustration, Branding</p>
     </WorkItem>
-    <WorkItem>
-      <p><Link to="/work/switching">Switching</Link></p>
+    <WorkItem projectPath="/work/switching" heroImagePath={switchingImage}>
+      <h2>Switching</h2>
+      <p>Interaction design, Product management</p>
     </WorkItem>
-    <WorkItem>
-      <p><Link to="/work/snapping">Snapping</Link></p>
+    <WorkItem projectPath="/work/snapping" heroImagePath={snappingImage}>
+      <h2>Snapping</h2>
+      <p>Interaction design, Product management, Design guideline development</p>
     </WorkItem>
+
   </Layout>
 ;
 
