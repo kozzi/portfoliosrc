33c9aaa786752646b8e096e303b79575106683d9
Charing Wong <miss@charingwong.com>
2016-07-21 12:16:47 -0400

    Add Feature work section on home page

--- pages/home/FeaturedWork.js
+++ pages/home/FeaturedWork.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Link from '../../components/Link';
+import WorkItem from '../work/WorkItem';
 import s from './FeaturedWork.css';
+import scandiImage from '../work/scandi/hero2.png';
+import deweyImage from '../work/dewey/hero.jpg';
 
 const FeaturedWork = () =>
   <section className={s.featuredWork}>
     <h1>Featured Work</h1>
+    <WorkItem projectPath="/work/scandi" heroImagePath={scandiImage}>
+      <h2>Team Scandi</h2>
+      <p>Illustration, YouTube channel design</p>
+    </WorkItem>
+    <WorkItem projectPath="/work/dewey" heroImagePath={deweyImage}>
+      <h2>Dewey</h2>
+      <p>iOS development (Swift), Interaction design, Illustration, Branding</p>
+    </WorkItem>
+    <p className={s.more}><Link to="/work">View full portfolio &#8594;</Link></p>
   </section>
 ;
 
